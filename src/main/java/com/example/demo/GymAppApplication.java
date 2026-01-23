6b67174af222bca8965c1c5abc5ba7ab1b9e498e
Vladyslav Dorosh <vladyslav.dorosh@student.tuke.sk>
2026-01-23 11:04:58 +0100

    add clock bean

--- src/main/java/com/example/demo/GymAppApplication.java
+++ src/main/java/com/example/demo/GymAppApplication.java
@@ -2,9 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Clock;
 
 @SpringBootApplication
 public class GymAppApplication {
+
+	@Bean
+	public Clock clock() {
+		return Clock.systemDefaultZone();
+	}
 
 	static void main(String[] args) {
 		SpringApplication.run(GymAppApplication.class, args);
